771f903aacf3816b27ac9b90eea6d9a5506c0013
Michael Prendergast <michael.prendergast50@gmail.com>
2024-09-05 14:58:43 +1200

    export AudioResponseSegment struct

--- audio.go
+++ audio.go
@@ -58,26 +58,28 @@ type AudioResponseWord struct {
 	End   float64 `json:"end"`
 }
 
+type AudioResponseSegment struct {
+	ID               int                 `json:"id"`
+	Seek             int                 `json:"seek"`
+	Start            float64             `json:"start"`
+	End              float64             `json:"end"`
+	Text             string              `json:"text"`
+	Tokens           []int               `json:"tokens"`
+	Temperature      float64             `json:"temperature"`
+	AvgLogprob       float64             `json:"avg_logprob"`
+	CompressionRatio float64             `json:"compression_ratio"`
+	NoSpeechProb     float64             `json:"no_speech_prob"`
+	Transient        bool                `json:"transient"`
+	Words            []AudioResponseWord `json:"words"`
+}
+
 // AudioResponse represents a response structure for audio API.
 type AudioResponse struct {
-	Task     string  `json:"task"`
-	Language string  `json:"language"`
-	Duration float64 `json:"duration"`
-	Segments []struct {
-		ID               int                 `json:"id"`
-		Seek             int                 `json:"seek"`
-		Start            float64             `json:"start"`
-		End              float64             `json:"end"`
-		Text             string              `json:"text"`
-		Tokens           []int               `json:"tokens"`
-		Temperature      float64             `json:"temperature"`
-		AvgLogprob       float64             `json:"avg_logprob"`
-		CompressionRatio float64             `json:"compression_ratio"`
-		NoSpeechProb     float64             `json:"no_speech_prob"`
-		Transient        bool                `json:"transient"`
-		Words            []AudioResponseWord `json:"words"`
-	} `json:"segments"`
-	Text string `json:"text"`
+	Task     string                 `json:"task"`
+	Language string                 `json:"language"`
+	Duration float64                `json:"duration"`
+	Segments []AudioResponseSegment `json:"segments"`
+	Text     string                 `json:"text"`
 
 	httpHeader
 }
